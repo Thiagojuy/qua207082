a9123f479a125dce18a8f14507bbc78d959eb86a
thiagojuy <thiagojuysg@gmail.com>
2025-09-10 20:01:28 -0300

    correção arquivo ex4_b.js

--- livrojs/cap04/js/ex4_b.js
+++ livrojs/cap04/js/ex4_b.js
@@ -1,19 +1,17 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-frm.addEventListener("submit",(e)=>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const velocPermitida = Number(frm.velocPermitida.value)
     const VelocCondutor = Number(frm.velocCondutor.value)
-    if(velocCondutor <= frm.velocPermitida.value){
+    if (velocCondutor <= frm.velocPermitida.value) {
         resp.innerText = `sem multa`
-    if(velocCondutor <= frm.velocPermitida.value * 1.2){
-        resp.innerText = `multa leve`
-    } else {
-        resp.innerText = `multa grave`
+        if (velocCondutor <= frm.velocPermitida.value * 1.2) {
+            resp.innerText = `multa leve`
+        } else {
+            resp.innerText = `multa grave`
+        }
     }
-}
-}
-}
 })
 
