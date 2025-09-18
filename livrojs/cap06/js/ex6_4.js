c27a1906030b356d586c0f8bfae89d12c9a9c5f7
thiagojuy <thiagojuysg@gmail.com>
2025-09-17 21:39:49 -0300

    aula 170925

---
+++ livrojs/cap06/js/ex6_4.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+const resp = document.querySelector('pre')
+const crincas = []  // declara vetor global
+
+// incluir crianças fixas
+crincas.push({nome: 'Lucas', idade: 4})
+crincas.push({nome: 'Ana', idade: 3})
+crincas.push({nome: 'Juliana', idade: 3}) 
+crincas.push({nome: 'Marina', idade: 4})
+crincas.push({nome: 'Bianca', idade: 6})
+crincas.push({nome: 'Pedro', idade: 6})
+crincas.push({nome: 'Cátia', idade: 6})
+
