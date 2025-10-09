205c3d9aad187e03214c2d9f80989b34cb167c1c
Thiagojuy <thiagojuysg@gmail.com>
2025-10-08 22:02:18 -0300

    Atualizar o script.js

--- exemplos/js/script.js
+++ exemplos/js/script.js
@@ -1,60 +1,81 @@
-// função para adicionar um valor ao visor
-function adicionar(valor) {
-    document.querySelector("#visor").value += valor
+const frm = document.querySelector("form")
+const tbody = document.querySelector("tbody")
+let lsItem = []
+let filtro = localStorage.getItem("filtro") 
+filtro = filtro == null ? "" : filtro
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const item = frm.inItem.value
+    const status = frm.inStatus.value
+    const index = frm.inIndex.value
+    // incluir ou atualizar
+    index == "" ? lsItem.push({item,status}) : lsItem[index] = {item,status}
+    atualizarTabela()
+})
+
+function prepararEdicao(index){
+    frm.inItem.value = lsItem[index].item
+    frm.inStatus.value = lsItem[index].status
+    frm.inIndex.value = index
+    frm.btApagar.disabled = false
 }
 
-
-//função para limpar o visor
-function limpar() {
-    document.querySelector("#visor").value = ""
-}
-
-// função para apagar o último caractere
-function apagar() {
-    const visor = document.querySelector("#visor")
-    visor.value = visor.value.slice(0, -1)
-}
-
-// função para calcular o resultado
-function calcular() {
-    const visor = document.querySelector("#visor")
-    try {
-        //usar a function eval() para calcular o resultado
-        visor.value = eval(visor.value)
-
-    } catch (error) {
-        visor.value = "erro"
-
-
+frm.btApagar.addEventListener("click", () => {
+    const index = frm.inIndex.value
+    if(index == ""){
+        alert("Necessário selecionar 1 item.")
+        return
     }
-}
-function porcentagem() {
-    calcular()
-    const visor = document.querySelector("#visor")
-    visor.value = Number(visor.value) / 100
-}
+    if(confirm("Deseja realmente apagar esse item?") == false){
+        return
+    }
+    lsItem.splice(index,1)
+    atualizarTabela() 
 
-const botoes = document.querySelectorAll("button")
-for (const botao of botoes) {
-    botao.addEventListener("click", () => {
+})
 
-        switch (botao.innerText) {
-            case "C":
-                limpar()
-                break;
-            case "←":
-                apagar()
-                break;
-            case "%":
-                porcentagem()
-                break;
-            case "=":
-                calcular()
-                break;
-            default:
-                adicionar(botao.innerText)
-                break;
+function atualizarTabela() {    
+    limpar()
+    localStorage.setItem("lsItem",JSON.stringify(lsItem))
+    tbody.innerHTML = ""
+    let cont = 0
+    for(i of lsItem){
+        if(filtro == "" || filtro.includes(i.status)){
+            tbody.innerHTML += 
+            `<tr onclick="prepararEdicao(${cont})" >
+                <td>${i.item}</td>
+                <td>${i.status}</td>
+            </tr>`
         }
+        cont++
+    }    
+}
+
+function limpar(){
+    frm.inItem.value = ""
+    frm.inStatus.value = ""
+    frm.inIndex.value = ""
+    frm.btApagar.disabled = true
+}
+
+if(localStorage.getItem("lsItem") != null){
+    lsItem = JSON.parse(localStorage.getItem("lsItem"))
+    atualizarTabela()
+}
+
+const lsFiltro = frm.querySelectorAll('input[type="checkbox"]')
+for(const bt of lsFiltro){
+    bt.addEventListener("click", filtrar)
+    if(filtro.includes(bt.value)){
+        bt.checked = true
     }
-    )
+}
+
+function filtrar(){  
+    filtro = ""  
+    for(const bt of lsFiltro){
+        filtro += bt.checked ? bt.value+"," : ""
+    }
+    atualizarTabela()
+    localStorage.setItem("filtro",filtro)
 }
