a4497c7e902bde8ff2eb4c1d7f3330bb2c5d489e
thiagojuy <thiagojuysg@gmail.com>
2025-10-22 21:44:17 -0300

    aula 221025

---
+++ exemplos/ecomerce/js/produtos.js
@@ -0,0 +1,71 @@
+const lsProduto = [
+    {
+
+        nome: "Banho de Mar",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+
+        nome: "Passeio na Floresta",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+
+        nome: "Trio Bom dia",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+        nome: "Meus Cristais",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+
+        nome: "Campo de Lavanda",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+
+        nome: "Minha Rotina",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+        nome: "Vida no Campo",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+
+        nome: "Meu Momento",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+
+    {
+
+        nome: "Flores Vivas",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    }
+
+
+]
