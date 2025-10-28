b55c810b09f5ac1c8810efad4b2caaba880db017
thiagojuy <thiagojuysg@gmail.com>
2025-10-27 21:47:10 -0300

    aula 271025

---
+++ Trabalho-Final/js/produto.js
@@ -0,0 +1,71 @@
+const lsProduto = [
+    {
+
+        nome: "Banho de Mar",
+        descricao: "Vela Aromática - 300g",
+        img: "img/chuteira1.jpeg",
+        valor: 59
+    },
+    {
+
+        nome: "Passeio na Floresta",
+        descricao: "Vela Aromática - 300g",
+        img: "img/chuteira2.jpeg",
+        valor: 39
+    },
+
+    {
+
+        nome: "Trio Bom dia",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-03.png",
+        valor: 99
+    },
+
+    {
+        nome: "Meus Cristais",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-04.png",
+        valor: 49
+    },
+
+    {
+
+        nome: "Campo de Lavanda",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-05.png",
+        valor: 39
+    },
+
+    {
+
+        nome: "Minha Rotina",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-06.png",
+        valor: 79
+    },
+
+    {
+        nome: "Vida no Campo",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-07.png",
+        valor: 89
+    },
+    {
+
+        nome: "Meu Momento",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-08.png",
+        valor: 79
+    },
+
+    {
+
+        nome: "Flores Vivas",
+        descricao: "Vela Aromática - 300g",
+        img: "img/vela-09.png",
+        valor: 69
+    }
+
+
+]
